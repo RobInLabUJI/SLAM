ce0fdf8408c41ed6e58859c56788f3d038d8cb2d
Enric Cervera <ecervera@uji.es>
2023-10-11 15:03:12 +0200

    Use host networking

--- Platforms/TurtleBot3/Gazebo/Noetic/run.bash
+++ Platforms/TurtleBot3/Gazebo/Noetic/run.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 rocker --env TURTLEBOT3_MODEL=${1:-burger} \
+       --network host \
        --nvidia --x11 tb3_gazebo:noetic \
        roslaunch turtlebot3_gazebo ${2:-turtlebot3_empty_world.launch}
