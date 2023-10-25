e5e34c79710ed917b79e63921aa0e8fbc553066c
Enric Cervera <ecervera@uji.es>
2023-10-25 12:07:08 +0200

    Set name of container

--- SLAM/SLAM_Toolbox/Foxy/run.bash
+++ SLAM/SLAM_Toolbox/Foxy/run.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-rocker --nvidia --x11 slam_toolbox:foxy \
+rocker --nvidia --x11 \
+       --name slam_toolbox slam_toolbox:foxy \
        ros2 launch slam_toolbox online_async_launch.py
