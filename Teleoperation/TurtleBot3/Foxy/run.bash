788e712598b154502aa0332f5d525690a92e442f
Enric Cervera <ecervera@uji.es>
2023-10-25 09:40:00 +0200

    Add name to container

--- Teleoperation/TurtleBot3/Foxy/run.bash
+++ Teleoperation/TurtleBot3/Foxy/run.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --env TURTLEBOT3_MODEL=${1:-burger} --rm -it tb3_teleop:foxy
+docker run --env TURTLEBOT3_MODEL=${1:-burger} \
+  --name tb3_teleop \
+  --rm -it tb3_teleop:foxy
