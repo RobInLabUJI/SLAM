473bab1b8cbd334c8b9b02247bcec37a0ccf5347
Enric Cervera <ecervera@uji.es>
2023-10-25 09:39:37 +0200

    Add name to container

--- Platforms/TurtleBot3/Gazebo/Foxy/run.bash
+++ Platforms/TurtleBot3/Gazebo/Foxy/run.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 rocker --env TURTLEBOT3_MODEL=${1:-burger} \
+	   --name tb3_gazebo \
        --nvidia --x11 tb3_gazebo:foxy \
        ros2 launch turtlebot3_gazebo ${2:-empty_world.launch.py}
