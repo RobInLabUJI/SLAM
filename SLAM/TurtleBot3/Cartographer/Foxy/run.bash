a837a8a2737281b8fef0244a63c61e9efd80d8c8
Enric Cervera <ecervera@uji.es>
2023-11-06 14:42:49 +0100

    Use default command

--- SLAM/TurtleBot3/Cartographer/Foxy/run.bash
+++ SLAM/TurtleBot3/Cartographer/Foxy/run.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 rocker --env TURTLEBOT3_MODEL=${1:-burger} \
-       --nvidia --x11 tb3_cartographer:foxy \
-       ros2 launch turtlebot3_cartographer cartographer.launch.py use_sim_time:=True
+       --nvidia --x11 tb3_cartographer:foxy
